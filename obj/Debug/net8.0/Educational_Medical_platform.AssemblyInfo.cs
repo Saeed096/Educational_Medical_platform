dcd388a0761975b73d0a93766142fdefbb5c90a2
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-09-17 22:01:35 +0300

    move commits from old branch

--- obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
+++ obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36bea63eb83db7bcb480ed457e113ec99a9e34b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f898d5c700499fd26493515cc904ab736af4567")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
