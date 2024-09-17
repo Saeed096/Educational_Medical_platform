25c4dea093b31427a4d50f5f03eadde43377e594
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-09-17 21:33:39 +0300

    test

--- obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
+++ obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06fbda2744b1f7e3f62f013c37abb81fdc52cc6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9fc22e7bc68c6d4e961fcbd914a17ae4959346")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
