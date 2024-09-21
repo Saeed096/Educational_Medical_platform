0325b7b9645656b536772aeb388f18c05021a1b5
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-09-21 09:34:53 +0300

    Question Description

--- obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
+++ obj/Debug/net8.0/Educational_Medical_platform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe243f638be7f9b0049178395c3c97f22ae2fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5d7fe71078a708e1794870c0e5616d027f0aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_Medical_platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
