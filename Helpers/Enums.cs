077b937da436ced2485cefff3797f8850b43285f
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-10-13 21:55:55 +0300

    Added Display names for Enums

--- Helpers/Enums.cs
+++ Helpers/Enums.cs
@@ -4,60 +4,78 @@ namespace Educational_Medical_platform.Helpers
 {
     public enum Gender
     {
+        [Display(Name = "Male")]
         Male = 0,
 
+        [Display(Name = "Female")]
         Female = 1,
     }
 
     public enum Role
     {
+        [Display(Name = "Student Role")]
         Student = 0,
 
+        [Display(Name = "Instructor Role")]
         Instructor = 1,
 
+        [Display(Name = "Admin Role")]
         Admin = 2,
     }
 
     public enum CourseType
     {
+        [Display(Name = "Free Course")]
         Free = 0,
 
+        [Display(Name = "Paid Course")]
         Paid = 1,
     }
 
     public enum CategoryType
     {
+        [Display(Name = "Course Category")]
         Courses = 0,
 
+        [Display(Name = "Book Category")]
         Books = 1,
 
+        [Display(Name = "Blog Category")]
         Blogs = 2,
 
+        [Display(Name = "Exam Category")]
         Exams = 3,
     }
 
     public enum SubCategoryType
     {
+        [Display(Name = "Course SubCategory")]
         Courses = 0,
 
+        [Display(Name = "Book SubCategory")]
         Books = 1,
 
+        [Display(Name = "Blog SubCategory")]
         Blogs = 2,
 
+        [Display(Name = "Exam SubCategory")]
         Exams = 3,
     }
 
     public enum CourseStatus
     {
+        [Display(Name = "Pending Approval")]
         PendingApproval = 0,
 
+        [Display(Name = "Approved")]
         Approved = 1,
 
+        [Display(Name = "Pending Deletion")]
         PendingDeletion = 2,
 
+        [Display(Name = "Rejected")]
         Rejected = 3,
     }
-
 
     public enum TestType
     {
@@ -79,6 +97,4 @@ namespace Educational_Medical_platform.Helpers
         [Display(Name = "Hard Level")]
         Hard = 2,
     }
-
-
 }
