f919d15b2911749cd6ae14cccdc2f1365afde78a
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-10-19 15:29:42 +0300

    Conmented Some Paypal mehtods

--- PayPal/PayPalClientApi.cs
+++ PayPal/PayPalClientApi.cs
@@ -226,108 +226,108 @@ namespace Educational_Medical_platform.PayPal
             return result;
         }
 
-        public async Task<bool> ActiveSubscription(string id, SubscriptionStatusChangeRequest request)
-        {
-            EnsureHttpClientCreated();
+    //    public async Task<bool> ActiveSubscription(string id, SubscriptionStatusChangeRequest request)
+    //    {
+    //        EnsureHttpClientCreated();
 
 
-            var requestContent = JsonConvert.SerializeObject(request);
+    //        var requestContent = JsonConvert.SerializeObject(request);
 
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
-            };
+    //        var httpRequestMessage = new HttpRequestMessage
+    //        {
+    //            Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
+    //        };
 
-            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/activate", httpRequestMessage.Content);
+    //        var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/activate", httpRequestMessage.Content);
 
-            return response.StatusCode == HttpStatusCode.NoContent;
-        }
+    //        return response.StatusCode == HttpStatusCode.NoContent;
+    //    }
 
-        public async Task<bool> SuspendSubscription(string id, SubscriptionStatusChangeRequest request)
-        {
-            EnsureHttpClientCreated();
+    //    public async Task<bool> SuspendSubscription(string id, SubscriptionStatusChangeRequest request)
+    //    {
+    //        EnsureHttpClientCreated();
 
-            var requestContent = JsonConvert.SerializeObject(request);
+    //        var requestContent = JsonConvert.SerializeObject(request);
 
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
-            };
+    //        var httpRequestMessage = new HttpRequestMessage
+    //        {
+    //            Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
+    //        };
 
-            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/suspend", httpRequestMessage.Content);
+    //        var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/suspend", httpRequestMessage.Content);
 
-            return response.StatusCode == HttpStatusCode.NoContent;
-        }
+    //        return response.StatusCode == HttpStatusCode.NoContent;
+    //    }
 
-        public async Task<bool> CancelSubscription(string id, SubscriptionStatusChangeRequest request)
-        {
-            EnsureHttpClientCreated();
+    //    public async Task<bool> CancelSubscription(string id, SubscriptionStatusChangeRequest request)
+    //    {
+    //        EnsureHttpClientCreated();
 
-            var requestContent = JsonConvert.SerializeObject(request);
+    //        var requestContent = JsonConvert.SerializeObject(request);
 
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
-            };
+    //        var httpRequestMessage = new HttpRequestMessage
+    //        {
+    //            Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
+    //        };
 
-            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/cancel", httpRequestMessage.Content);
+    //        var response = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/billing/subscriptions/{id}/cancel", httpRequestMessage.Content);
 
-            return response.StatusCode == HttpStatusCode.NoContent;
-        }
+    //        return response.StatusCode == HttpStatusCode.NoContent;
+    //    }
 
-        public async Task<bool> RefundSubscriptionAmount(string refundUrl, RefundRequest request)
-        {
-            EnsureHttpClientCreated();
+    //    public async Task<bool> RefundSubscriptionAmount(string refundUrl, RefundRequest request)
+    //    {
+    //        EnsureHttpClientCreated();
 
-            var requestContent = JsonConvert.SerializeObject(request);
+    //        var requestContent = JsonConvert.SerializeObject(request);
 
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
-            };
+    //        var httpRequestMessage = new HttpRequestMessage
+    //        {
+    //            Content = new StringContent(requestContent, Encoding.UTF8, "application/json")
+    //        };
 
-            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //        httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = await _client.PostAsync(refundUrl, httpRequestMessage.Content);
+    //        var response = await _client.PostAsync(refundUrl, httpRequestMessage.Content);
 
-            return response.StatusCode == HttpStatusCode.NoContent;
-        }
+    //        return response.StatusCode == HttpStatusCode.NoContent;
+    //    }
 
-        public async Task<bool> VerifyEvent(string json, IHeaderDictionary headerDictionary)
-        {
-            // !!IMPORTANT!!
-            // Without this direct JSON serialization, PayPal WILL ALWAYS return verification_status = "FAILURE".
-            // This is probably because the order of the fields are different and PayPal does not sort them. 
-            var paypalVerifyRequestJsonString = $@"{{
-				""transmission_id"": ""{headerDictionary["PAYPAL-TRANSMISSION-ID"][0]}"",
-				""transmission_time"": ""{headerDictionary["PAYPAL-TRANSMISSION-TIME"][0]}"",
-				""cert_url"": ""{headerDictionary["PAYPAL-CERT-URL"][0]}"",
-				""auth_algo"": ""{headerDictionary["PAYPAL-AUTH-ALGO"][0]}"",
-				""transmission_sig"": ""{headerDictionary["PAYPAL-TRANSMISSION-SIG"][0]}"",
-				""webhook_id"": ""6WC685942N447610S"",
-				""webhook_event"": {json}
-				}}";
+    //    public async Task<bool> VerifyEvent(string json, IHeaderDictionary headerDictionary)
+    //    {
+    //        // !!IMPORTANT!!
+    //        // Without this direct JSON serialization, PayPal WILL ALWAYS return verification_status = "FAILURE".
+    //        // This is probably because the order of the fields are different and PayPal does not sort them. 
+    //        var paypalVerifyRequestJsonString = $@"{{
+				//""transmission_id"": ""{headerDictionary["PAYPAL-TRANSMISSION-ID"][0]}"",
+				//""transmission_time"": ""{headerDictionary["PAYPAL-TRANSMISSION-TIME"][0]}"",
+				//""cert_url"": ""{headerDictionary["PAYPAL-CERT-URL"][0]}"",
+				//""auth_algo"": ""{headerDictionary["PAYPAL-AUTH-ALGO"][0]}"",
+				//""transmission_sig"": ""{headerDictionary["PAYPAL-TRANSMISSION-SIG"][0]}"",
+				//""webhook_id"": ""6WC685942N447610S"",
+				//""webhook_event"": {json}
+				//}}";
 
-            var content = new StringContent(paypalVerifyRequestJsonString, Encoding.UTF8, "application/json");
+    //        var content = new StringContent(paypalVerifyRequestJsonString, Encoding.UTF8, "application/json");
 
-            var resultResponse = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/notifications/verify-webhook-signature", content);
+    //        var resultResponse = await _client.PostAsync($"{ConfigHelper.BaseUrl}/v1/notifications/verify-webhook-signature", content);
 
-            var responseBody = await resultResponse.Content.ReadAsStringAsync();
+    //        var responseBody = await resultResponse.Content.ReadAsStringAsync();
 
-            var verifyWebhookResponse = JsonConvert.DeserializeObject<WebHookVerificationResponse>(responseBody);
+    //        var verifyWebhookResponse = JsonConvert.DeserializeObject<WebHookVerificationResponse>(responseBody);
 
-            if (verifyWebhookResponse.verification_status != "SUCCESS")
-            {
-                return false;
-            }
+    //        if (verifyWebhookResponse.verification_status != "SUCCESS")
+    //        {
+    //            return false;
+    //        }
 
-            return true;
-        }
+    //        return true;
+    //    }
     }
 }
