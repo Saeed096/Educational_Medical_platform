8f9fbe56cad039fcd11d32681ecd718802f98745
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-10-19 15:28:20 +0300

    Added UserSubscriptions table

---
+++ Models/UserSubscriptions.cs
@@ -0,0 +1,29 @@
+﻿using Shoghlana.Core.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Educational_Medical_platform.Models
+{
+    public class UserSubscriptions
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+       
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        //---------------------------------------------------
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        //---------------------------------------------------
+
+        public int SubscriptionPlanId { get; set; }
+    }
+}
