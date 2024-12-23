72056c05b27fccaec48a810da4b43f40675fd62c
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-12-23 09:01:09 +0200

    finalize cancel subscription logic

---
+++ Helpers/SubscriptionService.cs
@@ -0,0 +1,119 @@
+﻿namespace Educational_Medical_platform.Helpers
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        public SubscriptionService() { }
+
+
+        public DateTime getEnddate(DateTime enddate)
+        {
+            if (enddate.Day > DateTime.Today.Day)
+            {
+                // use isdayfoundinmonth before build date
+
+                if (isDayFoundInMonth(enddate.Day, DateTime.Today.Month))
+                {
+                    enddate = new DateTime(DateTime.Today.Year
+                    , DateTime.Today.Month, enddate.Day);
+                }
+                else
+                {
+                    if (DateTime.Today.Month != 12)
+                    {
+                        enddate = new DateTime(DateTime.Today.Year, DateTime.Today.Month + 1, 1);
+                    }
+                    else
+                    {
+                        enddate = new DateTime(DateTime.Today.Year + 1, 1, 1);
+
+                    }
+                }
+
+            }
+            else
+            {
+                if (DateTime.Today.Month != 12)
+                {
+                    if (isDayFoundInMonth(enddate.Day, DateTime.Today.Month + 1))
+                    {
+                        enddate = new DateTime(DateTime.Today.Year,
+                            DateTime.Today.Month + 1, enddate.Day);
+                    }
+                    else
+                    {
+                        if (DateTime.Today.Month != 11)
+                        {
+                            enddate = new DateTime(DateTime.Today.Year, DateTime.Today.Month + 2, 1);
+                        }
+                        else
+                        {
+                            enddate = new DateTime(DateTime.Today.Year + 1, 1, 1);
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (isDayFoundInMonth(enddate.Day, 1))
+                    {
+                        enddate = new DateTime(DateTime.Today.Year + 1,
+                      1, enddate.Day);
+                    }
+                    else
+                    {
+                        enddate = new DateTime(DateTime.Today.Year + 1, 2, 1);
+                    }
+
+                }
+            }
+            return enddate;
+        }
+
+
+
+        private bool isDayFoundInMonth(int day, int month)
+        {
+            int[] largeMonths = { 1, 3, 5, 7, 8, 10, 12 }; // months that contain 31 day
+            int[] smallMonths = { 4, 6, 9, 11 };  // months that contain 30 day
+
+            if (largeMonths.Contains(month))
+            {
+                return true;
+            }
+            else if (smallMonths.Contains(month))
+            {
+                if (day > 30)
+                {
+                    return false;
+                }
+                return true;
+            }
+            else // handle februaury case
+            {
+                if (DateTime.IsLeapYear(DateTime.Today.Year))
+                {
+                    if (day <= 29)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (day <= 28)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ PayPal/CancelSubscriptionWebhookEvent.cs
@@ -0,0 +1,104 @@
+﻿namespace Educational_Medical_platform.PayPal
+{
+    public class CancelSubscriptionWebhookEvent
+    {
+        public string id { get; set; }
+        public DateTime create_time { get; set; }
+        public string resource_type { get; set; }
+        public string event_type { get; set; }
+        public string summary { get; set; }
+        public Resource resource { get; set; }
+        public List<Link> links { get; set; }
+        public string event_version { get; set; }
+    }
+
+
+
+    public class AgreementDetails
+    {
+        public OutstandingBalance outstanding_balance { get; set; }
+        public string num_cycles_remaining { get; set; }
+        public string num_cycles_completed { get; set; }
+        public DateTime last_payment_date { get; set; }
+        public LastPaymentAmount last_payment_amount { get; set; }
+        public DateTime final_payment_due_date { get; set; }
+        public string failed_payment_count { get; set; }
+    }
+
+    public class Amount
+    {
+        public string value { get; set; }
+    }
+
+    public class ChargeModel
+    {
+        public string type { get; set; }
+        public Amount amount { get; set; }
+    }
+
+    public class LastPaymentAmount
+    {
+        public string value { get; set; }
+    }
+
+    public class MerchantPreferences
+    {
+        public SetupFee setup_fee { get; set; }
+        public string auto_bill_amount { get; set; }
+        public string max_fail_attempts { get; set; }
+    }
+
+    public class OutstandingBalance
+    {
+        public string value { get; set; }
+    }
+
+    public class Payer
+    {
+        public string payment_method { get; set; }
+        public string status { get; set; }
+        public PayerInfo payer_info { get; set; }
+    }
+
+    public class PayerInfo
+    {
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string payer_id { get; set; }
+        public ShippingAddress shipping_address { get; set; }
+    }
+
+    public class PaymentDefinition
+    {
+        public string type { get; set; }
+        public string frequency { get; set; }
+        public string frequency_interval { get; set; }
+        public Amount amount { get; set; }
+        public string cycles { get; set; }
+        public List<ChargeModel> charge_models { get; set; }
+    }
+
+    public class Plan
+    {
+        public string curr_code { get; set; }
+        public List<object> links { get; set; }
+        public List<PaymentDefinition> payment_definitions { get; set; }
+        public MerchantPreferences merchant_preferences { get; set; }
+    }
+
+    public class Resource
+    {
+        public AgreementDetails agreement_details { get; set; }
+        public string description { get; set; }
+        public List<Link> links { get; set; }
+        public string id { get; set; }
+        public ShippingAddress shipping_address { get; set; }
+        public string state { get; set; }
+        public Plan plan { get; set; }
+        public Payer payer { get; set; }
+        public DateTime start_date { get; set; }
+    }
+
+
+}
